f00e51a5201703682b19168151589c47d5120926
Alexander Ivanov <ttecss@gmail.com>
2016-03-28 16:36:02 +0200

    Added the option to insert a comment from VBOX7 video sharing website.

--- src/forplay.js
+++ src/forplay.js
@@ -1,3 +1,21 @@
+function replaceText(text, oTextHandle)
+{
+    var begin = oTextHandle.value.substr(0, oTextHandle.selectionStart);
+    var end = oTextHandle.value.substr(oTextHandle.selectionEnd);
+    var scrollPos = oTextHandle.scrollTop;
+    oTextHandle.value = begin + text + end;
+    if (oTextHandle.setSelectionRange) {
+        oTextHandle.focus();
+        oTextHandle.setSelectionRange(begin.length + text.length, begin.length + text.length);
+    }
+    oTextHandle.scrollTop = scrollPos;
+}
+
+function rand(arr)
+{
+    return arr[Math.round( Math.random() * (arr.length - 1) )];
+}
+
 (function ($) {
 	// Cached variables
 	var youtubeRegex = /https?:\/\/(www\.|m.)?(youtube\.com|youtu\.be)\/(embed\/|v\/|watch\?(.*&)?v=)([\w-]+)/;
@@ -21,6 +39,22 @@
 				);
 			}
 		});
+
+        // Let's add comment "generation" from Vbox7.com
+        $('.button_submit').parent().prepend($('<input type="submit" value="Вмъкни VBOX7 коментар" onclick="return false;" class="button_submit vbox7">').click(function() {
+            $.get('http://vbox7.com/ajax/home/popular.php', function(data) {
+                var mdkey = rand(data.items).mdkey;
+                $.get('http://vbox7.com/ajax/nginx/aj_comment_get_json_v1.php', {mdkey: mdkey}, function(data) {
+                    var pagination = data.pop();
+                    if (!data.length) {
+                        $('.button_submit.vbox7').click();
+                        return;
+                    }
+                    var comment = rand(data).d.comment;
+                    replaceText(comment, $('textarea[name=message]')[0]);
+                }, 'json');
+            });
+        }));
     });
 
 
