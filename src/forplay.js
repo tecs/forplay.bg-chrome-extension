01234ded3461dc0bbdfc0244ec5e8416f2c638ba
Alexander Ivanov <ttecss@gmail.com>
2018-07-20 02:56:16 +0300

    Converted all tabs into spaces

--- src/forplay.js
+++ src/forplay.js
@@ -17,28 +17,28 @@ function rand(arr)
 }
 
 (function ($) {
-	// Cached variables
-	var youtubeRegex = /https?:\/\/(www\.|m.)?(youtube\.com|youtu\.be)\/(embed\/|v\/|watch\?(.*&)?v=)([\w-]+)/;
-	
+    // Cached variables
+    var youtubeRegex = /https?:\/\/(www\.|m.)?(youtube\.com|youtu\.be)\/(embed\/|v\/|watch\?(.*&)?v=)([\w-]+)/;
+
     //Run on jQuery ready
     $(document).ready(function () {
-		// First of all, let's make that logo useful
-		$('#logo a').attr('href', 'http://forplay.bg/forums/index.php');
-		
-		// As for links...
-		$('.post .inner a').each(function() {
-			// Open in new window
-			$(this).attr('target', '_blank');
-			
-			// Embed youtube links
-			if (youtubeRegex.test(this.href)) {
-				var url = 'http://www.youtube.com/embed/' + youtubeRegex.exec(this.href).pop() + '?rel=0';
-				$(this).after(
-					$('<br>'),
-					$('<iframe title="YouTube video player" width="640" height="385" frameborder="0" allowfullscreen="">').attr('src', url)
-				);
-			}
-		});
+        // First of all, let's make that logo useful
+        $('#logo a').attr('href', 'http://forplay.bg/forums/index.php');
+
+        // As for links...
+        $('.post .inner a').each(function() {
+            // Open in new window
+            $(this).attr('target', '_blank');
+
+            // Embed youtube links
+            if (youtubeRegex.test(this.href)) {
+                var url = 'http://www.youtube.com/embed/' + youtubeRegex.exec(this.href).pop() + '?rel=0';
+                $(this).after(
+                    $('<br>'),
+                    $('<iframe title="YouTube video player" width="640" height="385" frameborder="0" allowfullscreen="">').attr('src', url)
+                );
+            }
+        });
 
         // Let's add comment "generation" from vbox7.com
         $('.button_submit').parent().prepend($('<input type="submit" value="VBOX7" onclick="return false;" class="button_submit vbox7">').click(function() {
@@ -78,6 +78,4 @@ function rand(arr)
             });
         }));
     });
-
-
 })(jQuery);
